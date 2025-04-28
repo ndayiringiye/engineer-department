735cd0b48ca0e2656be6a44e9a716756ff561895
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-04-28 17:33:22 +0200

    react setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet esse nisi possimus libero eum impedit voluptas placeat maxime repellendus a rem repudiandae consectetur dolores, totam maiores sit accusantium natus soluta.</p>
+    </div>
+  )
+}
+
+export default App
